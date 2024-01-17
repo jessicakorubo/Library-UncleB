8d7be0425f2e74eba4356534f2bee4d6d4a0e1f6
Jessica <jessicakorubo@gmail.com>
2024-01-17 08:20:54 +0100

    Added complete imported sqql file

--- sql.sql
+++ sql.sql
@@ -1,66 +1,246 @@
-create table if not exists `books` (
-    `book_id` INT not null AUTO_INCREMENT PRIMARY KEY,
-    `title` varchar(255) not null,
-    `author` varchar(255) not null,
-    `genre` varchar(100) not null,
-    `edition` varchar(100),
-    `date_published` date,
-    `copies` int(10) not null,
-    `status` varchar(100) not null
-)ENGINE InnoDB DEFAULT CHARSET = utf8;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 17, 2024 at 08:18 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
-create table if not exists `members` (
-    `member_id` int(11) not null,
-    `name` varchar(100),
-    `gender` varchar(2) not null,
-    `registration_date` date not null,
-    `email` varchar(100) not null,
-    `phone_number` int,
-    `borrowed_books` int,
-    PRIMARY KEY (`member_id`)
-)ENGINE InnoDB DEFAULT CHARSET = utf8;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-create table if not exists `book_copies` (
-    `copy_id` INT AUTO_INCREMENT,
-    `book_id` INT NOT NULL,
-    `ISBN` INT NOT NULL PRIMARY KEY,
-    status ENUM('Available', 'Issued', 'Lost') NOT NULL,
-    UNIQUE KEY unique_copy_name (`copy_id`),
-    FOREIGN KEY (`book_id`) REFERENCES books(`book_id`)
-)ENGINE InnoDB DEFAULT CHARSET = utf8;
 
-create table if Nnot exists `issued-books` (
-    `member_id` int(11) not null,
-    `member_name` varchar(255) not null,
-    `ISBN` INT NOT NULL PRIMARY KEY,
-    `book_name` varchar(255) not null,
-    `author` varchar(100) not null,
-    `date_issued` date not null,
-    `due_date` date not null,
-    `status` varchar(50) not null,
-    `copies` int(11) not null,
-    `image` varchar(255) not null
-    FOREIGN KEY (`ISBN`) REFERENCES book_copies(`ISBN`)
-)ENGINE InnoDB DEFAULT CHARSET = utf8;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-create table if not exists `admin` ( 
-    `admin_id` int not null AUTO_INCREMENT, 
-    `admin_username` varchar(100) NOT NULL, 
-    `admin_password` varchar(100) NOT NULL,
-    PRIMARY KEY (`admin_id`) 
-)ENGINE InnoDB DEFAULT CHARSET=utf8;
+--
+-- Database: `library`
+--
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `admin_username` varchar(100) NOT NULL,
+  `admin_password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `admin_username`, `admin_password`) VALUES
+(1, 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `book_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `genre` varchar(100) NOT NULL,
+  `edition` varchar(100) DEFAULT NULL,
+  `date_published` date DEFAULT NULL,
+  `copies` int(10) NOT NULL,
+  `status` varchar(100) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`book_id`, `title`, `author`, `genre`, `edition`, `date_published`, `copies`, `status`, `image`) VALUES
+(1, 'Purple Hibiscus', 'Chimamanda Ngozi Adichie', 'Fiction', '1st Edition', '2016-02-18', 3, 'Available', 'purpleHibiscus.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book_copies`
+--
+
+CREATE TABLE `book_copies` (
+  `copy_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `ISBN` int(11) NOT NULL,
+  `status` enum('Available','Issued','Lost') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `book_copies`
+--
+
+INSERT INTO `book_copies` (`copy_id`, `book_id`, `ISBN`, `status`) VALUES
+(3, 1, 16502, 'Available'),
+(2, 1, 63091, 'Available'),
+(1, 1, 88052, 'Available');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `issued_books`
+--
+
+CREATE TABLE `issued_books` (
+  `member_id` int(11) NOT NULL,
+  `member_name` varchar(255) NOT NULL,
+  `ISBN` int(11) NOT NULL,
+  `book_name` varchar(255) NOT NULL,
+  `author` varchar(100) NOT NULL,
+  `date_issued` date NOT NULL,
+  `due_date` date NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `copies` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE `members` (
+  `member_id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `gender` varchar(2) NOT NULL,
+  `registration_date` date NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone_number` int(11) DEFAULT NULL,
+  `borrowed_books` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `members`
+--
+
+INSERT INTO `members` (`member_id`, `name`, `gender`, `registration_date`, `email`, `phone_number`, `borrowed_books`) VALUES
+(42231, 'Adaora Akpan', 'f', '2024-01-01', 'adaoraakpan@gmail.com', 2147483647, NULL),
+(792830, 'Miriam Chimezie', 'f', '2024-01-01', 'miriamchimezie@gmail.com', 903392345, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `returned_books`
+--
+
+CREATE TABLE `returned_books` (
+  `book_name` varchar(255) NOT NULL,
+  `return_id` int(11) NOT NULL,
+  `ISBN` int(11) DEFAULT NULL,
+  `due_date` date NOT NULL,
+  `date_issued` date NOT NULL,
+  `date_returned` date NOT NULL,
+  `member_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`book_id`);
+
+--
+-- Indexes for table `book_copies`
+--
+ALTER TABLE `book_copies`
+  ADD PRIMARY KEY (`ISBN`),
+  ADD UNIQUE KEY `unique_copy_name` (`copy_id`),
+  ADD KEY `book_id` (`book_id`);
+
+--
+-- Indexes for table `issued_books`
+--
 ALTER TABLE `issued_books`
-ADD FOREIGN KEY (`ISBN`)
-REFERENCES `book_copies`(`ISBN`);
+  ADD PRIMARY KEY (`ISBN`);
 
+--
+-- Indexes for table `members`
+--
+ALTER TABLE `members`
+  ADD PRIMARY KEY (`member_id`);
 
+--
+-- Indexes for table `returned_books`
+--
+ALTER TABLE `returned_books`
+  ADD PRIMARY KEY (`return_id`),
+  ADD KEY `ISBN` (`ISBN`);
 
-create table if not exists `returned_books` (
-    `return_id` int not null AUTO_INCREMENT PRIMARY KEY,
-    `ISBN` int,
-    `date_issued` date not null,
-    `date_returned` date not null,
-    `member_id` int (11) not null,
-    FOREIGN KEY (`ISBN`) REFERENCES book_copies(`ISBN`)
-)ENGINE InnoDB DEFAULT CHARSET=utf8;
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `book_copies`
+--
+ALTER TABLE `book_copies`
+  MODIFY `copy_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `returned_books`
+--
+ALTER TABLE `returned_books`
+  MODIFY `return_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `book_copies`
+--
+ALTER TABLE `book_copies`
+  ADD CONSTRAINT `book_copies_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`);
+
+--
+-- Constraints for table `issued_books`
+--
+ALTER TABLE `issued_books`
+  ADD CONSTRAINT `issued_books_ibfk_1` FOREIGN KEY (`ISBN`) REFERENCES `book_copies` (`ISBN`),
+  ADD CONSTRAINT `issued_books_ibfk_2` FOREIGN KEY (`ISBN`) REFERENCES `book_copies` (`ISBN`),
+  ADD CONSTRAINT `issued_books_ibfk_3` FOREIGN KEY (`ISBN`) REFERENCES `book_copies` (`ISBN`);
+
+--
+-- Constraints for table `returned_books`
+--
+ALTER TABLE `returned_books`
+  ADD CONSTRAINT `returned_books_ibfk_1` FOREIGN KEY (`ISBN`) REFERENCES `book_copies` (`ISBN`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
